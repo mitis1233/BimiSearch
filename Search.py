54103ae6afd15216f4ec63b209c8a36cdba0952f
mitis1233 <mtis1233@gmail.com>
2022-03-29 13:52:59 +0800

    修正成bimibimi新網址

--- Search.py
+++ Search.py
@@ -2,7 +2,7 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import requests,re,sys,time,win32gui,win32api,win32con
 from bs4 import BeautifulSoup
-from untitled import Ui_Form
+from MainUI import Ui_Form
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -17,15 +17,15 @@ class MainWindow(QMainWindow):
         self.header={
             'Connection': 'keep-alive',
             'DNT': '1',
-            'Host': 'www.bimiacg.net',
-            'Referer': 'http://www.bimiacg.net/bangumi/',
+            'Host': 'uos.starpark.me',
+            'Referer': 'https://www.bimiacg4.net/bangumi/',
             'Upgrade-Insecure-Requests': '1',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
             }
         self.header2={
                 'DNT': '1',
                 'Range': 'bytes=0-',
-                'Host': '49.234.56.246',
+                'Host': 'www.bimiacg4.net',
                 'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
                 'sec-ch-ua-mobile': '?0',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
@@ -40,8 +40,8 @@ class MainWindow(QMainWindow):
             self.ui.lineEdit_2.clear()
             self.ui.lineEdit_3.setText('網址輸入錯誤')
         Referer=re.sub('/play', '', Referer)
-        if re.compile(r'www.bimiacg.net').findall(URL)!=[]:
-            Referer='http://www.bimiacg.net/bangumi/'+Referer
+        if re.compile(r'www.bimiacg4.net').findall(URL)!=[]:
+            Referer='https://www.bimiacg4.net/bangumi/'+Referer
         elif re.compile(r'm.bimibimi.cc').findall(URL)!=[]:
             Referer='http://m.bimibimi.cc/bangumi/'+Referer
             self.header["Host"]='m.bimibimi.cc'
@@ -54,30 +54,32 @@ class MainWindow(QMainWindow):
             
         self.header["Referer"]=Referer
         try:
-            r = requests.get(URL,headers=self.header)
+            r = requests.get(URL,headers=self.header,verify=False)
             script = BeautifulSoup(r.text,"html.parser").find_all('div','play-player')
             uid=re.compile(r'"url":".+"').findall(str(script))[0]
             uid=re.sub('","url_next":.+', '', uid)
             uid=re.sub('"url":"', '', uid)
-            VideoHtml='http://49.234.56.246/danmu/play.php?url='+uid+'&myurl='+URL
-            self.ui.lineEdit_2.setText(VideoHtml)
-            r = requests.get(VideoHtml,headers=self.header2)
+            VideoHtml='https://www.bimiacg4.net/static/danmu/play.php?url='+uid+'&myurl='+URL
+            
+            r = requests.get(VideoHtml,headers=self.header2,verify=False)
             soup = BeautifulSoup(r.text,"html.parser")
             script = soup.select("video")
             uid=re.compile(r'src=".+"').findall(str(script))[0]
             uid=re.sub('" type.+', '', uid)
             VideoHtml2=re.sub('src="', '', uid)
+            self.ui.lineEdit_2.setText(VideoHtml2)
             self.ui.lineEdit_3.setText(VideoHtml2)
         except Exception as err:
             try:
-                VideoHtml='http://49.234.56.246/danmu/m3u8.php?url='+uid+'&myurl='+URL
-                self.ui.lineEdit_2.setText(VideoHtml)
-                r = requests.get(VideoHtml,headers=self.header2)
+                VideoHtml='https://www.bimiacg4.net/static/danmu/play.php?url='+uid+'&myurl='+URL
+                
+                r = requests.get(VideoHtml,headers=self.header2,verify=False)
                 soup = BeautifulSoup(r.text,"html.parser")
                 script = soup.select("video")
                 uid=re.compile(r'src=".+"').findall(str(script))[0]
                 uid=re.sub('" type.+', '', uid)
                 VideoHtml2=re.sub('src="', '', uid)
+                self.ui.lineEdit_2.setText(VideoHtml2)
                 self.ui.lineEdit_3.setText(VideoHtml2)
             except:
                 self.ui.lineEdit_2.clear()
